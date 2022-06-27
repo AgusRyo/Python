0c616b7909085f8376afabdcc2682838a91de9e3
AgusRyo <ableagus2010@gmail.com>
2022-06-26 22:18:17 -0300

    Fin de esta tanda

--- Ejercicio1.py
+++ Ejercicio1.py
@@ -67,6 +67,48 @@ interes=float(input("Ingrese el interes "))
 años=int(input("¿Por cuantos años? "))
 retorno=round(monto*(interes/100+1)**años,2)
 print("El capital de retorno es: $"+str(retorno))
+print("")
+'''
+Una juguetería tiene mucho éxito en dos de sus productos: payasos y muñecas. Suele hacer venta por correo y la
+empresa de logística les cobra por peso de cada paquete así que deben calcular el peso de los payasos y muñecas 
+que saldrán en cada paquete a demanda. Cada payaso pesa 112 g y cada muñeca 75 g. Escribir un programa que lea
+el número de payasos y muñecas vendidos en el último pedido y calcule el peso total del paquete que será 
+enviado
+'''
+dolls=int(input("Ingrese cantidad de muñecas vendida "))
+clown=int(input("Ingrese la cantidad de payasos vendidos "))
+peso_dolls=dolls*75
+peso_clown=clown*112
+print("El peso total del paquete es de: "+str(peso_dolls+peso_clown)+"grs")
+print("")
 
+'''
+Imagina que acabas de abrir una nueva cuenta de ahorros que te ofrece el 4% de interés al año. 
+Estos ahorros debido a intereses, que no se cobran hasta finales de año, se te añaden al balance final de tu
+cuenta de ahorros. Escribir un programa que comience leyendo la cantidad de dinero depositada en la cuenta de
+ahorros, introducida por el usuario. Después el programa debe calcular y mostrar por pantalla la cantidad de 
+ahorros tras el primer, segundo y tercer años. Redondear cada cantidad a dos decimales.
+'''
+monto=float(input("Ingrese el monto a invertir "))
+interes=0.04
+primer_cierre=monto*(1+interes)
+segundo_cierre=primer_cierre*(1+interes)
+tercer_cierre=segundo_cierre*(1+interes)
+print("Retorno del primer año: $"+str(round(primer_cierre,2)))
+print("Retorno del segundo año: $"+str(round(segundo_cierre,2)))
+print("Retorno del tercer año: $"+str(round(tercer_cierre,2)))
+print("")
 
+'''
+Una panadería vende barras de pan a 3.49$ cada una. El pan que no es el día tiene un descuento del 60%. 
+Escribir un programa que comience leyendo el número de barras vendidas que no son del día. 
+Después el programa debe mostrar el precio habitual de una barra de pan, el descuento que se le hace por no 
+ser fresca y el coste final total.
+'''
 
+pan_ayer=int(input("Ingrese la cantidad de pan de ayer vendido "))
+precio=3.49
+descuento=0.6
+precio_final=((pan_ayer*precio)*descuento)
+print("Precio habitual: $"+str(precio)+" descuento %60, precio final: $"+ str(precio_final))
+print("")

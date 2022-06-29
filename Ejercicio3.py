bbbbe4a556bf68c95a03442f0cdfd74c4b08e32a
AgusRyo <ableagus2010@gmail.com>
2022-06-28 23:18:01 -0300

    Condicionales simples

--- Ejercicio3.py
+++ Ejercicio3.py
@@ -10,3 +10,58 @@ else:
 
 print("")
 
+'''
+Escribir un programa que almacene la cadena de caracteres contraseña en una variable, pregunte al usuario por 
+la contraseña e imprima por pantalla si la contraseña introducida por el usuario coincide con la guardada en 
+la variable sin tener en cuenta mayúsculas y minúsculas.
+'''
+password1= "holamundo"
+ingreso=input("Ingrese la contraseña: ")
+
+if password1 == ingreso.lower():
+    print("Bienvenido Usuario")
+else:
+    print("Contraseña Incorrecta")
+print("")
+'''
+Escribir un programa que pida al usuario dos números y muestre por pantalla su división. 
+Si el divisor es cero el programa debe mostrar un error.
+'''
+
+num1=int(input("Ingrese el dividendo: "))
+num2=int(input("Ingrese otro divisor: "))
+
+if num2 != 0:
+    print(num1/num2)
+else:
+    print("No se puede dividir")
+print("")
+'''
+Escribir un programa que pida al usuario un número entero y muestre por pantalla si es par o impar.
+'''
+
+num= int(input("Ingrese un numero para saber si es par o impar: "))
+
+if num%2==0:
+    print("Es par")
+else:
+    print("Es impar")
+
+print("")
+
+'''
+Para tributar un determinado impuesto se debe ser mayor de 16 años y tener unos ingresos iguales o superiores a
+$1000 mensuales. Escribir un programa que pregunte al usuario su edad y sus ingresos mensuales y muestre por 
+pantalla si el usuario tiene que tributar o no.
+'''
+
+ingresos= float(input("Ingrese el valor de sus ingresos mensuales: "))
+edad= int(input("Ingrese su edad: "))
+
+if ingresos >=1000 and edad>16:
+    print("A usted le corresponde pagar impuestos")
+else: 
+    print("Usted está excento de impuestos")
+
+
+print("")

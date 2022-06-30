e6c3e53c95030488040ee61e9b0273057b9d5074
AgusRyo <ableagus2010@gmail.com>
2022-06-29 22:45:07 -0300

    Condicionales encadenados

--- Ejercicio3.py
+++ Ejercicio3.py
@@ -65,3 +65,27 @@ else:
 
 
 print("")
+
+'''
+Los alumnos de un curso se han dividido en dos grupos A y B de acuerdo al sexo y el nombre.
+El grupo A esta formado por las mujeres con un nombre anterior a la M y los hombres con un nombre posterior a
+la N y el grupo B por el resto. Escribir un programa que pregunte al usuario su nombre y sexo, y muestre por 
+pantalla el grupo que le corresponde.
+'''
+
+nombre=input("Ingrese su nombre: ")
+sexo=input("Ingrese su sexo  H/M: ")
+
+inicial=nombre[0]
+
+if sexo=="M":
+    if nombre.lower() <'m':
+        print("Tu grupo es A")
+    else:
+        print("Tu grupo es B")
+else:
+    if nombre.lower()> 'n':
+        print("Tu grupo es A")
+    else:
+        print("Tu grupo es B")
+print("")
